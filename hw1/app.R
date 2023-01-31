a9653e36c0eed849780f71553bb461ede9cc8efc
c-sabin <97855633+c-sabin@users.noreply.github.com>
2023-01-30 23:00:28 -0500

    change display of graphs (side by side in UI)

--- hw1/app.R
+++ hw1/app.R
@@ -59,8 +59,10 @@ ui <- fluidPage(
             
         #### MAIN PANEL ####
         mainPanel(
-           plotOutput("annual_emissions"),
-           plotOutput("annual_consumption")
+           fluidRow(
+             column(6, plotOutput("annual_emissions")),
+              column(6, plotOutput("annual_consumption"))
+             )
         )
     )
 )
@@ -69,23 +71,23 @@ ui <- fluidPage(
     
 server <- function(input, output) {
 
-      # Create a scatterplot of total annual emissions (in tons)
+      # Create a scatterplot of total annual emissions
       output$annual_emissions <- renderPlot({
         ggplot(data = waste_tons, aes(x = year, y = total_emissions)) + 
           geom_col() + 
           labs(title ="Annual Greenhouse Gas Emissions in Washington D.C.",
                x = "Year", y = "Emissions (millions of tons)") + 
-          scale_y_continuous(labels = label_number(scale = 0.000001)) + 
+          scale_y_continuous(labels = label_number(accuracy = .1, scale = 0.000001)) + 
           theme_classic()
       })
       
-      # Create a scatterplot of total annual consumption (in tons)  
+      # Create a scatterplot of total annual consumption 
       output$annual_consumption <- renderPlot({
         ggplot(data = waste_tons, aes(x = year, y = total_consumption)) + 
           geom_col() +
           labs(title ="Annual Waste Consumption in Washington D.C.",
                x = "Year", y = "Waste (millions of tons)") + 
-          scale_y_continuous(labels = label_comma(scale = 0.000001)) + 
+          scale_y_continuous(labels = label_number(accuracy = .1, scale = 0.000001)) +
           theme_classic()
       })
 }
